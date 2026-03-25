0500a5c25e37c879574dc529bd0943b592c85d73
Anton Lennström <antonlen@kth.se>
2026-03-25 11:35:27 +0100

    Add obj file reading

--- src/file_parser.rs
+++ src/file_parser.rs
@@ -1,3 +1,7 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::path::Path;
+
 pub struct Vertix{
     x: f32,
     y: f32,
@@ -16,10 +20,42 @@ pub struct Mesh {
     faces: Vec<Face>,
 }
 
-pub fn file_parse_interface(filename: String){
+pub fn file_parse_interface(filename: &str){
+    if !filename.ends_with(".obj"){
+        return;
+    }
 
+
+    let result = parse_obj_file(filename);
 }
 
-fn parse_obj_file(filepath: &str){
+fn parse_obj_file(filename: &str) -> io::Result<()> {
+    let path = Path::new(filename);
+    let file = File::open(&path)?;
+    let reader = BufReader::new(file);
 
+    let mut vertices: Vec<Vertix> = vec![]; 
+    let mut faces: Vec<Face> = vec![];
+    let mut objects: Vec<Mesh> = vec![];
+
+    for line_result in reader.lines(){
+        let line = line_result?;
+        let trimmed_line = line.trim();
+
+        match trimmed_line.to_lowercase() {
+            x if x.starts_with("f ") => {
+
+            },
+            x if x.starts_with("v ") => {
+
+            },
+            x if x.starts_with("o ") => {
+
+            },
+            _ => {},
+        }
+
+    }
+
+    Ok(())
 }
