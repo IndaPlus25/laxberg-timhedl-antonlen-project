51aca6163aaf14a3a9e715fbe0393411e650d72a
Anton Lennström <antonlen@kth.se>
2026-03-25 11:41:30 +0100

    Add obj face parsing

--- src/file_parser.rs
+++ src/file_parser.rs
@@ -26,6 +26,32 @@ pub struct Face {
     v3: usize,
 }
 
+impl Face {
+    fn parse(vertices: &str)  -> Option<Vec<Face>> {
+        let mut parts = vertices.split_whitespace();
+
+        let a = parts.next()?.split('/').next()?.parse::<usize>().ok()? - 1;
+        let b = parts.next()?.split('/').next()?.parse::<usize>().ok()? - 1;
+        let c = parts.next()?.split('/').next()?.parse::<usize>().ok()? - 1;
+
+        let d_option = parts.next()
+            .and_then(|s| s.split('/').next())
+            .and_then(|s| s.parse::<usize>().ok());
+
+        if let Some(d) = d_option{
+            let d = d - 1;
+            return Some(vec![
+                Face {v1: a, v2: b, v3: c},
+                Face {v1: a, v2: c, v3: d},   
+            ])
+        }
+
+        Some(vec![
+            Face {v1: a, v2: b, v3: c}
+        ])
+    }
+}
+
 pub struct Mesh {
     name: String,
     vertices: Vec<Vertix>,
@@ -55,7 +81,12 @@ fn parse_obj_file(filename: &str) -> io::Result<()> {
 
         match trimmed_line.to_lowercase() {
             x if x.starts_with("f ") => {
+                let raw_vertices = x[2..].trim();
 
+                match Face::parse(raw_vertices) {
+                    Some(mut f) => faces.append(&mut f),
+                    None => {println!("Error: corrupted parse")} // PLACEHOLDER ERROR
+                }
             },
             x if x.starts_with("v ") => {
                 let coordinates = x[2..].trim();
