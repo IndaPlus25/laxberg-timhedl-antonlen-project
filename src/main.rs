7164fc114a603fb34323eca7893a21bd1bf1e2d3
Anton Lennström <antonlen@kth.se>
2026-03-30 12:15:43 +0200

    cleanup: remove accidental test code in main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
-use crate::file_parser::file_parse_interface;
-
 mod file_parser;
 
 fn main() {
-    if let Some(mesh) = file_parse_interface("bugatti.obj"){        
-        for object in mesh{
-            println!("{}", object.name)
-        }
-    } else {
-        println!("Fail ;c")
-    };
+    println!("Hello, world!");
 }
