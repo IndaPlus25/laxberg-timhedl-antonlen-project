642362714ac6d46a3d0ba92b18ad7dee308cde4f
Tim-Hedlund <tim.hedlund@elev.ga.ntig.se>
2026-04-01 17:44:07 +0200

    feature: raycasting correctly

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,11 @@ struct IntersectionData {
     voxel_data: u32,
 }
 
+struct Player {
+    position: V3,
+    direction: (f32, f32),
+}
+
 //32x32x32 chunk
 struct Chunk {
     //first 8 bits are bools for children(1) existing in each of the 8 positions. Z-order curve
@@ -262,6 +267,28 @@ fn vec_exit_plane(v1: &V3) -> u32 {
     }
 }
 
+fn vec_normalize (v: &V3) -> V3 {
+    let len = (v.x * v.x + v.y * v.y + v.z * v.z).sqrt();
+
+    if len == 0.0 {
+        return V3 { x: 0.0, y: 0.0, z: 0.0 };
+    }
+
+    V3 {
+        x: v.x / len, 
+        y: v.y / len, 
+        z: v.z / len
+    }
+}
+
+fn vec_crossp(v1: &V3, v2: &V3) -> V3 {
+    V3 {
+        x: v1.y * v2.z - v1.z * v2.y,
+        y: v1.z * v2.x - v1.x * v2.z,
+        z: v1.x * v2.y - v1.y * v2.x
+    }
+}
+
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         if self.window.is_none() {
@@ -299,7 +326,38 @@ impl ApplicationHandler for App {
                     let width = size.width;
                     let height = size.height;
 
-                    default_color(&mut buffer, width, height);
+                    let mut tree_data = vec![0_u32; 40];
+
+                    let fov = std::f32::consts::PI / 2.0;
+
+                    let player = Player {
+                        position: V3{
+                            x: -32.0,
+                            y: -16.0,
+                            z: 30.0,
+                        },
+                        direction: (std::f32::consts::PI / 1.5, 0.2)               
+                    };
+
+                    tree_data[0] = (1 << 24) | (1 << (3 + 24)) | (1 << (6 + 24)) | 1; 
+
+                    tree_data[1] = (128 << 24) | 4; 
+                    tree_data[2] = (255 << 24) | (255 << 16) | 2; // Green block
+                    tree_data[3] = (255 << 24) | (255 << 16) | 3; // Blue block
+
+
+                    tree_data[4] = (128 << 24) | 5;
+                    tree_data[5] = (128 << 24) | 6;
+                    tree_data[6] = (128 << 24) | 7;
+                    tree_data[7] = (105 << 24) | (105 << 16) | 1;
+
+                    let chunk = Chunk {
+                        data: tree_data,
+                        min_pos: V3 { x: 0.0, y: 0.0, z: 0.0 },
+                        max_pos: V3 { x: 32.0, y: 32.0, z: 32.0 },
+                    };
+
+                    raycaster(&mut buffer, width, height, fov, player, &chunk);
                     
                     buffer.present().unwrap();
                 }
@@ -324,6 +382,67 @@ fn default_color(buffer: &mut [u32], width: u32, height: u32) {
 
         // Format: 0000_RRRR_GGGG_BBBB
         *pixel = (r << 16) | (g << 8) | b;
+    }
+}
+
+
+
+fn raycaster(buffer: &mut [u32], width: u32, height: u32, fov: f32, player: Player, chunk: &Chunk) {
+
+    let aspect_ratio = width as f32/height as f32;
+
+    let plane_width = 2.0 * (fov / 2.0).tan();
+    let plane_height = plane_width / aspect_ratio;
+
+    let global_up = V3 { x: 0.0, y: 0.0, z: 1.0 };
+
+    let forward_vec = V3 {
+        x: (player.direction.1).cos() * (player.direction.0).sin(),
+        y: (player.direction.1).sin(),
+        z: (player.direction.1).cos() * (player.direction.0).cos()
+    };
+
+    let right_vec = vec_normalize(&vec_crossp(&global_up, &forward_vec));
+
+    let up_vec = vec_normalize(&vec_crossp(&forward_vec, &right_vec));
+
+    let top_left_vec = vec_add(
+        &vec_sub(&forward_vec, &vec_mult_scal(&right_vec, plane_width / 2.0)), 
+        &vec_mult_scal(&up_vec, plane_height / 2.0)
+    );
+
+    let step_x_size = plane_width / width as f32;
+    let step_y_size = plane_height / height as f32;
+    
+    let delta_x = vec_mult_scal(&right_vec, step_x_size);
+    let delta_y = vec_mult_scal(&up_vec, -step_y_size);
+
+    for (index, pixel) in buffer.iter_mut().enumerate() {
+        let x = (index % width as usize) as f32;
+        let y = (index / width as usize) as f32;
+
+        let x_offset = vec_mult_scal(&delta_x, x);
+        let y_offset = vec_mult_scal(&delta_y, y);
+
+        let big_ray_dir = vec_add(&top_left_vec, &vec_add(&x_offset, &y_offset));
+
+        let ray_dir = vec_normalize(&big_ray_dir);
+
+        let ray = Ray { origin: player.position, direction: ray_dir };
+
+        let intersection = find_intersection(&ray, chunk, chunk.data[0]);
+
+        if let Some(hit) = intersection {
+            let b = get_ending(hit.voxel_data);
+            *pixel = match b {
+                1 => 0xFF0000, // Red
+                2 => 0x00FF00, // Green
+                3 => 0x0000FF, // Blue
+                _ => 0xFFFFFF, // White
+            };
+        } else {
+            *pixel = 0; // Your gray/blue background
+        }
     }
 }
 
