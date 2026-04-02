1b0effd5018530ff852bd795ceb06316ee53d77a
Lucas Axberg <lucas.axberg@gmail.com>
2026-04-02 09:52:14 +0200

    improve: change edge plane function to require less calculations in comparisons

--- src/voxelizer.rs
+++ src/voxelizer.rs
@@ -67,72 +67,38 @@ fn vertecies_outside_same_face(vertecies: [[f32; 3]; 3], cube_center: [f32; 3], 
 }
 
 fn vertecies_outside_same_edge(vertecies: [[f32; 3]; 3], cube_center: [f32; 3], cube_width: f32) -> bool {
-    let min_x = cube_center[0] - cube_width;
-    let max_x = cube_center[0] + cube_width;
-
-    let min_y = cube_center[1] - cube_width;
-    let max_y = cube_center[1] + cube_width;
-
-    let min_z = cube_center[2] - cube_width;
-    let max_z = cube_center[2] + cube_width;
+    let threshold = 2.0 * cube_width;
 
     // Array of possible faces, remains true if all vertecies are outside it
     let mut outsides = [true; 12];
 
     for vertex in vertecies {
-        let x = vertex[0];
-        let y = vertex[1];
-        let z = vertex[2];
+        let dx = vertex[0] - cube_center[0];
+        let dy = vertex[1] - cube_center[1];
+        let dz = vertex[2] - cube_center[2];
 
         // Planes independent of the z-axis
-        if x+y <= max_x + max_y {
-            outsides[0] = false;
-        }
-        if x-y <= max_x + min_y {
-            outsides[1] = false;
-        }
-        if -x+y <= min_x + max_y{
-            outsides[2] = false;
-        }
-        if -x-y <= -1.0 * (min_x + min_y) {
-            outsides[3] = false;
-        }
-
+        if dx + dy <= threshold { outsides[0] = false; }
+        if dx - dy <= threshold { outsides[1] = false; }
+        if -dx + dy <= threshold { outsides[2] = false; }
+        if -dx - dy <= threshold { outsides[3] = false; }
+        
         // Planes independent of the x-axis
-        if y+z <= max_y + max_z {
-            outsides[4] = false;
-        }
-        if y-z <= max_y + min_z {
-            outsides[5] = false;
-        }
-        if -y+z <= min_y + max_z {
-            outsides[6] = false;
-        }
-        if -y-z <= -1.0 * (min_y + min_z) {
-            outsides[7] = false;
-        }
+        if dy + dz <= threshold { outsides[4] = false; }
+        if dy - dz <= threshold { outsides[5] = false; }
+        if -dy + dz <= threshold { outsides[6] = false; }
+        if -dy - dz <= threshold { outsides[7] = false; }
 
         // Planes independent of the y-axis
-        if x+z <= max_x + max_z {
-            outsides[8] = false;
-        }
-        if x-z <= max_x + min_z {
-            outsides[9] = false;
-        }
-        if -x+z <= min_x + max_z {
-            outsides[10] = false;
-        }
-        if -x-z <= -1.0 * (min_x + min_z) {
-            outsides[11] = false;
-        }
+        if dx + dz <= threshold { outsides[8] = false; }
+        if dx - dz <= threshold { outsides[9] = false; }
+        if -dx + dz <= threshold { outsides[10] = false; }
+        if -dx - dz <= threshold { outsides[11] = false; }
+
         
     }
 
-    if outsides.iter().any(|x| *x == true ) {
-        return true;
-    }
-
-    return false;
+    outsides.iter().any(|x| *x )
 }
 
 fn vertecies_outside_same_corner(vertecies: [[f32; 3]; 3], cube_center: [f32; 3], cube_width: f32) -> bool {
