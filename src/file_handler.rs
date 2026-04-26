9e7f130c8fb4664f360427121cc689ef3412d3b3
Anton Lennström <antonlen@kth.se>
2026-04-26 15:11:19 +0200

    Fixes #33

--- src/file_handler.rs
+++ src/file_handler.rs
@@ -1,5 +1,8 @@
 use std::collections::HashMap;
 use std::fmt::Error;
+use serde::{Serialize, Deserialize};
+use std::fs::File;
+use std::io::{BufReader, BufWriter};
 
 use crate::vecmath::*;
 use crate::octree::*;
@@ -10,8 +13,19 @@ pub fn save_file_interface(data: HashMap<V3i, Chunk>) -> Result<(), Error>{
     Ok(())
 }
 
+fn save_file(filepath: &str) -> Result<(), Box<dyn std::error::Error>>{
+    let file = File::create(filepath)?;
+    let writer = BufWriter::new(file);
+
+    let data: Vec<u32> = vec![];
+
+    bincode::serialize_into(writer, &data)?;
+    Ok(())
+}
+
 
 /// this is the interface to interact with the file_handler when loading a file.
 pub fn load_file_interface(file: String) -> Result<(), Error>{
     Ok(())
 }
+
