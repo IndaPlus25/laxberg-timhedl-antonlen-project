1288bc0fdd47f343b4f437ed34374b2905e9d1e0
Lucas Axberg <lucas.axberg@gmail.com>
2026-05-07 20:16:09 +0200

    feature: continuous noise between chunks by using global coordinates

--- src/worldgen.rs
+++ src/worldgen.rs
@@ -1,22 +1,31 @@
 use rand::RngExt; 
 use noise::{NoiseFn, Perlin};
+use crate::vecmath::V3i;
 
 
-pub fn generate_single_chunk(color: u32, seed: u32) -> Vec<u32> {
+pub fn generate_single_chunk(color: u32, seed: u32, chunk_coord: &V3i) -> Vec<u32> {
     let mut flat_data = vec![0; 32768];
     let perlin = Perlin::new(seed);
     let scale = 0.01;
 
     for dx in 0..32 {
         for dz in 0..32 {
-            let noise_value = (perlin.get([scale * dx as f64 , scale * dz as f64]) + 1.0) / 2.0;
-            let limit = (16.0 + (noise_value * 16.0)) as u32;
-            println!("{}", noise_value);
+
+            let global_x = chunk_coord.x * 32 + dx;
+            let global_z = chunk_coord.z * 32 + dz;
+
+            let noise_x = global_x as f64 * scale;
+            let noise_z = global_z as f64 * scale;
+            let noise_value = (perlin.get([noise_x, noise_z]) + 1.0) / 2.0;
+
+            let global_y_limit = (16.0 + (noise_value * 16.0)) as i32;
 
             for dy in 0..32 {
                 let index = dx + (dy * 32) + (dz * 32 * 32);
-                if (dy as u32) < limit{
-                    flat_data[index] = color;
+
+                let global_y = chunk_coord.y * 32 + dy;
+                if global_y < global_y_limit {
+                    flat_data[index as usize] = color;
                 }
             }
         }
